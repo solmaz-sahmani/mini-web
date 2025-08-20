b80832611b04ba8edb7186c60f11c1ba43ca5631
solmaz-sahmani <solmaz.sahmani@gmail.com>
2025-08-20 10:35:51 +0330

    initial Swagger setup

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_yasg',
     'home.apps.HomeConfig',
     'about.apps.AboutConfig',
     'contactus.apps.ContactusConfig',
